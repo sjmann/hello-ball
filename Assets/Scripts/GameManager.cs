43719e1b3374802d3989571f556c73304018155b
Mann Steven <steven.mann@buhlergroup.com>
2018-06-07 16:09:44 +0200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static int score;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+        score = 0;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        setCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            GameManager.score++;
+            setCountText();
+        }
+
+        if (other.gameObject.CompareTag("Killer"))
+        {
+            GameOver();
+        }
+
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                gameObject.SetActive(false);
+                winText.text = "!!!YOU WIN!!! Score: " + GameManager.score.ToString();
+            }
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + GameManager.score.ToString();
+    }
+
+    void GameOver()
+    {
+        winText.text = "!!!YOU DIE!!!";
+        gameObject.SetActive(false);
+        SceneManager.LoadScene(0);
+    }
+}
